1c24a05f013c1b1561f346a976a2a3302bd9b425
Mohamedsabry109 <mohamedsabry109@gmail.com>
2020-10-17 18:56:19 +0200

    updating conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('../appeal_rl/dqn'))
 sys.path.insert(0, os.path.abspath('../appeal_rl/env'))
-sys.path.insert(0, os.path.abspath('../appeal_rl/dqn'))
+sys.path.insert(0, os.path.abspath('../appeal_rl'))
 #sys.path.insert(0, os.path.abspath('/home/mohamed/Appeal '))
 
 
