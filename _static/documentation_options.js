aadcd742283c2464fa9680c3a4c8faecba09e1c0
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-10-18 18:23:17 +0000

    Update docs after building Travis build 35 of Mohamedsabry109/Appeal
    The docs were built from the branch 'master' against the commit
    6343779a148522b4605f74fa3621492ebd06ddaf.
    
    The Travis build that generated this commit is at
    https://travis-ci.com/Mohamedsabry109/Appeal/jobs/401724914.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post40+ge2a9a87',
+    VERSION: '0.post41+g6343779',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
