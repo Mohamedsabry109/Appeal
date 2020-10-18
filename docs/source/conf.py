49c61ce321937a7387c436cab804b5806e03bfde
Mohamedsabry109 <mohamedsabry109@gmail.com>
2020-10-18 18:21:23 +0200

    editing docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,8 +19,10 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('..'))
+# sys.path.insert(0, os.path.abspath('.'))
+# sys.path.insert(0, os.path.abspath('..'))
+sys.path.append(os.path.abspath('.'))
+sys.path.append(os.path.abspath('..'))
 # sys.path.insert(0, os.path.abspath('../../'))
 # sys.path.insert(0, os.path.abspath('../appeal_rl/dqn'))
 # sys.path.insert(0, os.path.abspath('../appeal_rl/env'))
