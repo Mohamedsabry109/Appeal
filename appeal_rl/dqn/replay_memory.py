92850da95c73d56e31ccac89bc24b47e1c91adf1
Mohamedsabry109 <mohamedsabry109@gmail.com>
2020-10-20 19:05:50 +0200

    adding docs for the dqn module

--- appeal_rl/dqn/replay_memory.py
+++ appeal_rl/dqn/replay_memory.py
@@ -11,12 +11,26 @@ class ReplayMemory:
         self.buffer = deque(maxlen=size)
 
     def clear(self):
+        """
+        Clear the buffer
+        """
         self.buffer.clear()
 
     def add(self, state, vark_action, taxonomies_action, reward, next_state, done):
+        """
+        add state, action, reward, next state and done tuple to the buffer
+        """
         self.buffer.appendleft((state, vark_action, taxonomies_action, reward, next_state, done))
 
     def sample(self, batch_size):
+        """
+        Sample amini batch of a certain size from the buffer
+
+        Paramters
+        ---------
+
+        batch_size : size of the mini batch to be sampled 
+        """
         return random.sample(self.buffer, batch_size)
 
     @property
