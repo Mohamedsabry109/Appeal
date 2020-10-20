94c18ce45ea4c8c5d62667cfbab81c3f30286951
Mohamedsabry109 <mohamedsabry109@gmail.com>
2020-10-20 18:58:33 +0200

    adding documentation for the dqn model

--- appeal_rl/dqn/model.py
+++ appeal_rl/dqn/model.py
@@ -4,10 +4,22 @@ import torch.nn as nn
 class Model(nn.Module):
     def __init__(self, in_dim, num_taxonomies, delta_difficulties=True):
         """
-        :param in_dim: Observation space dimensionality
-        :param num_taxonomies: Number of taxonomies
-        :param delta_difficulties: If true, then 5 Q-values representing the possible changes in difficulties will be
-         predicted. Else, 3 Q-values representing the absolute difficulties will be predicted.
+        Parameters
+        ----------
+        in_dim : int
+            input dimension, observations dimension 
+        num_taxonomies : int
+            number of taxonomies, 6 in our case
+
+        delta_difficulties : boolean
+            True is we uses delta_difficulties, i.e 5 levels to represent difficulty, E, EM, M, MH, H or 3 levels E, M, H
+            If true, then 5 Q-values representing the possible changes in difficulties will be
+            predicted. Else, 3 Q-values representing the absolute difficulties will be predicted.
+
+        Returns
+        -------
+
+
         """
         super(Model, self).__init__()
         # Core dimensions are the inner dimensions of the neural network.
@@ -30,6 +42,20 @@ class Model(nn.Module):
         self.taxonomy_terminals = nn.ModuleList([nn.Linear(core_dims[-1], dim_taxonomy) for _ in range(self.num_taxonomies)])
 
     def forward(self, *input):
+        """
+        Parameters
+        ----------
+        input : numpy array
+            observation  
+
+
+        Returns
+        -------
+        vark_head : numpy array of dimension equals to VARK dimension
+
+        taxonomy_heads : numpy array of shape equals to number of taxonomies * 5 if delta_difficulties is True, 3 otherwise
+
+        """
         net = self.core(input[0])
         vark_head = self.vark_terminal(net)
         taxonomy_heads = []
